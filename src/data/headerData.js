e3ed7d2f6932b4d548615e0c15be828440e825f9
VIvekkumar Singh <88674179+Mr-Vivekkumar@users.noreply.github.com>
2024-01-22 02:41:20 +0530

    final

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ export const headerData = {
   desciption:
     "Hello! I am Vivekkumar Singh, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
   // image: "https://i.ibb.co/chMyysx/Whats-App-Image-2024-01-04-at-1-30-54-PM-removebg-preview-removebg-preview-1.png",
-  image: "https://i.ibb.co/x60kjcn/Portfolio-img-removebg-preview.png",
+  image: "https://i.ibb.co/SwmQ5k5/Linkedien-pic-removebg-preview-1.png",
   
   //imagebw: "https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg",
   resumePdf:
